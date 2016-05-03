1faae613ccb8bfafd69ea1d6ca3f5f6fa9f4fb65
Nicholas Roberts <niccolo.roberts@gmail.com>
2016-05-02 23:00:17 -0700

    removed pip

--- install-ansible-ppa.sh
+++ install-ansible-ppa.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 #curl https://raw.githubusercontent.com/ErgonLogicEnterprises/ansible-bootstrap/master/install-ansible-ppa.sh | /bin/sh
-sudo apt-get install git python-setuptools python-dev python-pip python-crypto -yq
+sudo apt-get install git python-setuptools python-dev -yq
 sudo apt-get install software-properties-common -yq
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
